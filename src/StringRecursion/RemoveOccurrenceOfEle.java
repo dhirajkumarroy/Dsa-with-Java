74f606d90bc482973dee49da430621acc593b479
dhirajkumarroy <dheerajkumar.ara1111@gmail.com>
2025-10-19 01:09:41 +0530

    add new file

--- src/StringRecursion/RemoveOccurrenceOfEle.java
+++ src/StringRecursion/RemoveOccurrenceOfEle.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class RemoveOccurrenceOfEle {
 
+    static String remove_occurrence_of_ele2(String str,char ch){
+        if(str.isEmpty()){
+            return " ";
+        }
+
+        String smallans = remove_occurrence_of_ele2(str.substring(1),ch);
+
+        char current = str.charAt(0);
+
+        if(current != ch){
+            return current +smallans;
+        }
+        else{
+            return smallans;
+        }
+    }
+
     static String remove_occurrence_of_ele(String st ,int idx,char ch){
 
        if(idx == st.length()){
@@ -29,7 +46,7 @@ public class RemoveOccurrenceOfEle {
         String st = sc.nextLine();
 
         System.out.println(remove_occurrence_of_ele(st,0,'a'));
-
+        System.out.println(remove_occurrence_of_ele2(st,'a'));
 
 
     }
---
+++ src/StringRecursion/reverseOfStr.java
@@ -0,0 +1,35 @@
+package StringRecursion;
+
+import java.util.Scanner;
+
+public class reverseOfStr {
+
+    static String reverse_of_str2(String st,int idx){
+        if(idx == st.length()){
+            return "";
+        }
+        String smallans = reverse_of_str2(st,idx+1);
+        char current = st.charAt(idx);
+        return smallans + current;
+    }
+   static String reverse_of_str(String st){
+       if(st.isEmpty()){
+           return "";
+       }
+       String smallans = reverse_of_str(st.substring(1));
+       char current = st.charAt(0);
+       return smallans + current;
+   }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(reverse_of_str(str));
+
+        System.out.println(reverse_of_str2(str,0));
+        System.out.println(str);
+    }
+}
+
