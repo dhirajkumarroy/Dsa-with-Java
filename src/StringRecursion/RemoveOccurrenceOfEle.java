d43394e8c44015d9cc166a7bd52f07714489b229
dhirajkumarroy <dheerajkumar.ara1111@gmail.com>
2025-10-18 22:34:15 +0530

    add recursion file

---
+++ src/StringRecursion/RemoveOccurrenceOfEle.java
@@ -0,0 +1,37 @@
+package StringRecursion;
+
+import java.util.Scanner;
+
+public class RemoveOccurrenceOfEle {
+
+    static String remove_occurrence_of_ele(String st ,int idx,char ch){
+
+       if(idx == st.length()){
+           return "";
+       }
+
+       String small = remove_occurrence_of_ele(st,idx+1,ch);
+
+       char current = st.charAt(idx);
+
+       if(current != ch){
+           return current + small;
+       }
+       else{
+           return small;
+       }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+
+        System.out.println(remove_occurrence_of_ele(st,0,'a'));
+
+
+
+    }
+
+}
